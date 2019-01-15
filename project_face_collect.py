4f2f70a0546d7018d65e14d9a702a384da015527
jainanushree96 <46053822+jainanushree96@users.noreply.github.com>
2019-01-15 07:40:45 +0000

    FACE RECOGNITION USING KNN

---
+++ project_face_collect.py
@@ -0,0 +1,38 @@
+import cv2
+import numpy as np
+cap=cv2.VideoCapture(0)
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+skip=0
+face_data=[]
+datasetpath='./data/'
+file_name=input("enter name of person:")
+while True:
+    ret,frame=cap.read()
+    if ret==False:
+        continue
+    faces=face_cascade.detectMultiScale(frame,1.3,5)
+    if len(faces)==0:
+        continue
+    faces=sorted(faces,key=lambda f:f[2]*f[3])
+    for face in faces[-1:]:
+        x,y,w,h=face
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,255,255),2)
+        offset=10
+        face_section=frame[y-offset:y+h+offset,x-offset:x+w+offset]
+        face_section=cv2.resize(face_section,(100,100))
+        skip+=1
+        if skip%10==0:
+            face_data.append(face_section)
+            print(len(face_data))
+    cv2.imshow("frame",frame)
+    cv2.imshow("face section",face_section)
+    key_pressed=cv2.waitKey(1) & 0XFF
+    if key_pressed == ord('q'):
+        break
+face_data=np.asarray(face_data)
+face_data=face_data.reshape((face_data.shape[0],-1))
+print(face_data.shape)
+np.save(datasetpath+file_name+'.npy',face_data)
+
+cap.release()
+cv2.destroyAllWindows()
